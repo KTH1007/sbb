151b34dfcceef86a6b87141da95fde083d2cca3b
KTH1007 <rlaxogjs2684@gmail.com>
2024-07-24 16:01:45 +0900

    Feat: 부분 문자열로 제목 조회

--- src/main/java/com/example/sbb/question/repository/QuestionRepository.java
+++ src/main/java/com/example/sbb/question/repository/QuestionRepository.java
@@ -3,6 +3,9 @@ package com.example.sbb.question.repository;
 import com.example.sbb.question.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+    List<Question> findBySubjectLike(String subject);
 }
