89b83b1a7b78c41e5f8cde9c268169d13db72584
KTH1007 <rlaxogjs2684@gmail.com>
2024-07-25 15:18:38 +0900

    Feat: 상세 페이지 이동

--- src/main/java/com/example/sbb/question/service/QuestionService.java
+++ src/main/java/com/example/sbb/question/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.example.sbb.question.service;
 
+import com.example.sbb.global.exception.DataNotFoundException;
 import com.example.sbb.question.domain.Question;
 import com.example.sbb.question.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -19,6 +21,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 
 }
