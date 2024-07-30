6f2fc9123cbd5bbc99d34dfb8a8ecb335f4f271f
KTH1007 <rlaxogjs2684@gmail.com>
2024-07-30 17:03:52 +0900

    Feat: 로그인, 로그아웃 기능

--- src/main/java/com/example/sbb/member/repository/MemberRepository.java
+++ src/main/java/com/example/sbb/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.sbb.member.repository;
 import com.example.sbb.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByMembername(String membername);
 }
