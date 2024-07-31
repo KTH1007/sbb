627a636ddd21b910c702f266d8dd61b57494ce49
KTH1007 <rlaxogjs2684@gmail.com>
2024-07-31 11:07:12 +0900

    Feat: 수정 일시 추가

--- src/main/java/com/example/sbb/answer/domain/Answer.java
+++ src/main/java/com/example/sbb/answer/domain/Answer.java
@@ -30,4 +30,6 @@ public class Answer {
 
     @ManyToOne
     private Member author;
+
+    private LocalDateTime modifyDate;
 }
--- src/main/java/com/example/sbb/question/domain/Question.java
+++ src/main/java/com/example/sbb/question/domain/Question.java
@@ -34,4 +34,6 @@ public class Question {
 
     @ManyToOne
     private Member author;
+
+    private LocalDateTime modifyDate;
 }
