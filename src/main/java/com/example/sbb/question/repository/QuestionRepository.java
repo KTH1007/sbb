e934e8529ac215ac5be20f31214c470e454b52ed
KTH1007 <rlaxogjs2684@gmail.com>
2024-08-01 16:59:43 +0900

    Refactor: @Query를 사용하여 검색

--- src/main/java/com/example/sbb/question/repository/QuestionRepository.java
+++ src/main/java/com/example/sbb/question/repository/QuestionRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +16,18 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Page<Question> findAll(Pageable pageable);
     Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+
+    @Query("select "
+            + "distinct q "
+            + "from Question q "
+            + "left outer join Member m1 on q.author=m1 "
+            + "left outer join Answer a on a.question=q "
+            + "left outer join Member m2 on a.author=m2 "
+            + "where "
+            + "   q.subject like %:kw% "
+            + "   or q.content like %:kw% "
+            + "   or m1.membername like %:kw% "
+            + "   or a.content like %:kw% "
+            + "   or m2.membername like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
